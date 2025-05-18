2dcce21a956e15c3ded13394127e21b4502085e8
edineiandrade <edinei22.ba@gmail.com>
2025-05-18 13:11:59 -0300

    configuração vercel: add local_run

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-from api.main import main
+from api.main import app
 
+# Esta parte é necessária para a Vercel
 if __name__ == "__main__":
-    main()
+    import uvicorn
+    uvicorn.run(app, host="0.0.0.0", port=8000)
